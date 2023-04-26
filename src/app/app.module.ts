16e6634bb187648d54a70f264df02884a673dc3d
ydroulis <lombardi.droulis@gmail.com>
2023-04-26 08:27:23 -0300

    add new components in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { CoursewareComponent } from './components/courseware/courseware.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
+import { HeaderMenuDarkComponent } from './components/header-menu-dark/header-menu-dark.component';
+import { HeaderMenuLightComponent } from './components/header-menu-light/header-menu-light.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    CoursewareComponent,
+    HeaderComponent,
+    HeaderMenuComponent,
+    HeaderMenuDarkComponent,
+    HeaderMenuLightComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule, CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
