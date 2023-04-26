f850d086d8bf6f50695eddffe1be1b7e7fcfb72e
ydroulis <lombardi.droulis@gmail.com>
2023-04-26 08:26:11 -0300

    set routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './authGuard';
+import { CoursewareComponent } from './components/courseware/courseware.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, },
+  { path: 'courseware', component: CoursewareComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
