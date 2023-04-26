a96fa6ed7990d3095f25a72dd3d2c95af2d967c9
ydroulis <lombardi.droulis@gmail.com>
2023-04-26 16:36:19 -0300

    adding new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from './authGuard';
 import { CoursewareComponent } from './components/courseware/courseware.component';
+import { AllDesignsComponent } from './components/all-designs/all-designs.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, },
-  { path: 'courseware', component: CoursewareComponent }
+  { path: 'courseware', component: CoursewareComponent },
+  { path: 'all-designs', component: AllDesignsComponent }
 ];
 
 @NgModule({
